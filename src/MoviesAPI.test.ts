f9173d1ab96b606a43933296604488fc3e7a4c01
David <learnitmyway@users.noreply.github.com>
2020-04-26 16:34:02 +0200

    Add detailed comments to test

--- src/MoviesAPI.test.ts
+++ src/MoviesAPI.test.ts
@@ -22,11 +22,18 @@ class MoviesAPIFake extends MoviesAPI {
 
 describe('MoviesAPI', () => {
   it('fetches all movies', async () => {
+    // We cannot stub a protected method,
+    // so we create a fake.
     const moviesAPI = new MoviesAPIFake()
+
+    // We create a stub because we don't
+    // want to call an external service.
+    // We also want to use it for testing.
     const getStub = (): Promise<Movie[]> =>
       Promise.resolve(moviesSample())
     moviesAPI.get = jest.fn(getStub)
 
+    // We use a test server instead of the actual one.
     const { query } = testServer(() => ({ moviesAPI }))
 
     const GET_MOVIES = gql`
@@ -38,11 +45,19 @@ describe('MoviesAPI', () => {
       }
     `
 
+    // A query is made as if it was a real service.
     const res = await query({ query: GET_MOVIES })
 
-    // This helps us see the errors if there are any
+    // We ensure that the errors are undefined.
+    // This helps us to see what goes wrong.
     expect(res.errors).toBe(undefined)
+
+    // We check to see if the `movies`
+    // endpoint is called properly.
     expect(moviesAPI.get).toHaveBeenCalledWith('movies')
+
+    // We check to see if we have
+    // all the movies in the sample.
     expect(res?.data?.movies).toEqual(moviesSample())
   })
 
